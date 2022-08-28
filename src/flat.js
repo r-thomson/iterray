f61e71f9d5aeac090e37c00a4bcdd3fb6ba11aeb
Ryan Thomson <git@ryanthomson.net>
2022-08-27 22:58:48 -0400

    Add documentation for flat function

--- src/flat.js
+++ src/flat.js
@@ -1,5 +1,16 @@
 const isIterable = (x) => x && typeof x[Symbol.iterator] === 'function';
 
+/**
+ * Flatten the given iterable up to a specified depth.
+ *
+ * Note that any sub-element that implements the interator protocol is
+ * eligible for recusirve flattening (arrays, strings, etc).
+ *
+ * @param {Iterable} iterable Source iterable.
+ * @param {Number} depth Number of levels to flatten.
+ * @yields {any} Values from the source after flattening.
+ * @see Array.prototype.flat
+ */
 export default function* flat (iterable, depth = 1) {
     for (const value of iterable) {
         if (depth > 0 && isIterable(value)) {
